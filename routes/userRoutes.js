31b0b35885081dc2cbc9d9f140afe9c6ec685aa2
tjdli <tristen.jd.li@gmail.com>
2023-02-11 17:58:55 -0500

    Updated userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,22 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.route("/login").get((req, res) => {
+  console.log(req.query);
+  User.findOne({username: req.body.username, password: req.query.password})
+    .then(user => {
+      //console.log(res.json(user)._id);
+      res.json(user)
+    })
+    .catch(err => res.status(400).json(`Error: ${err}`));
+})
+
 router.route("/add").post((req, res) => {
   const username = req.body.username;
   const email = req.body.email;
@@ -38,13 +54,24 @@ router.route("/update/:id").post((req, res) => {
             user.lastName = req.body.lastName;
             user.phoneNumber = req.body.phoneNumber;
             user.tags.push(req.body.tags.split(", "));
-            user.interests.push(req.body.interests.split(","));
+            user.plans.push(req.body.plans.split(","));
 
             user.save()
                 .then(() => res.json("Updated user!"))
                 .catch(err => res.status(400).json(`Error: ${err}`));
         })
         .catch(err => res.status(400).json(`Error: ${err}`));
+})
+
+router.route("/update/:id/addTag").post((req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      user.tags.push(req.body.tags.split(", "));
+      user.save()
+      .then(() => res.json("Updated user!"))
+      .catch(err => res.status(400).json(`Error: ${err}`));
+    })
+    .catch(err => res.status(400).json(`Error: ${err}`));
 })
 
 module.exports = router;
